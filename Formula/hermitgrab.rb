1197ec68896f7a3d992c3a6371d400042df5f25d
Karsten Becker <skipper.pasty.0a@icloud.com>
2025-07-20 12:46:29 +0200

    New formula that uses codesign and binaries

--- Formula/hermitgrab.rb
+++ Formula/hermitgrab.rb
@@ -1,14 +1,28 @@
 class Hermitgrab < Formula
-  desc "A tag-based configuration manager"
-  homepage "https://github.com/karstenb/hermitgrab"
-  url "https://github.com/karstenb/hermitgrab/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "5e8ca96d91227b87adb6ee3b9d31c036de8c8294260b30c66780e6b4efae1736"
-  license "GPL3"
+  desc "Minimal binary fetcher for hermit"
+  homepage "https://hermitgrab.app"
+  
+  if Hardware::CPU.arm?
+    url "https://hermitgrab.app/releases/latest/download/hermitgrab-macos-aarch64.zip"
+    sha256 "4befa8bfbdc222ad68470cd974d99a70e28e023d777dca2e777b138086702f7c"
+  else
+    url "https://hermitgrab.app/releases/latest/download/hermitgrab-macos-x86_64.zip"
+    sha256 "69eb39d806ed6bc2744c6537e26d7afeed90918b4682d7ba5f519f48e5cca9c8"
+  end
 
-  depends_on "rust" => :build
+  version "latest"
+  license "GPL-3.0-only"
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "hermitgrab"
+
+    # Attempt ad-hoc codesign
+    system "codesign", "--force", "--deep", "--sign", "-", bin/"hermitgrab"
+
+    # Optionally remove quarantine attribute
+    if MacOS.version >= :catalina
+      system "xattr", "-d", "com.apple.quarantine", bin/"hermitgrab"
+    end
   end
 
   test do
